0a0c1cce304d5d883f1cbe27983bacbafc2511f3
Anders Geil <anders.geil@gmail.com>
2017-02-13 11:05:28 +0100

    FizzBuzz according to formal requirements

---
+++ Opgave 1/Exercise1/FizzBuzz/Buzzer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace FizzBuzz
+{
+	public class Buzzer
+	{
+		public static string Buzz(int i)
+		{
+			if (i % 3 == 0 && i % 5 == 0)
+			{
+				return "FizzBuzz";
+			}
+			else if (i % 3 == 0)
+			{
+				return "Fizz";
+			}
+			else if (i % 5 == 0)
+			{
+				return "Buzz";
+			}
+			else
+			{
+				return i.ToString();
+			}
+		}
+	}
+}
