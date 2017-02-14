be31b744d788e831c49f59a47b1cce526625dfa6
Satannius <tobiasostannius@gmail.com>
2017-02-14 12:09:15 +0100

    Fuldendt NUnit tests.

--- Opgave 1/Exercise1/Tests/Test.cs
+++ Opgave 1/Exercise1/Tests/Test.cs
@@ -1,14 +1,61 @@
 ﻿using NUnit.Framework;
 using System;
+using FizzBuzz;
+
 namespace Tests
 {
 	[TestFixture()]
 	public class Test
 	{
+		/* Smoke tests */
 		[Test()]
-		public void TestCase()
+		public void Test3YieldsFizz()
 		{
-			Assert.AreEqual(7, 7);
+			Assert.AreEqual(Buzzer.Buzz(3), "Fizz");
+		}
+		[Test()]
+		public void Test5YieldsBuzz()
+		{
+			Assert.AreEqual(Buzzer.Buzz(5), "Buzz");
+		}
+		[Test()]
+		public void Test15YieldsFizzBuzz()
+		{
+			Assert.AreEqual(Buzzer.Buzz(15), "FizzBuzz");
+		}
+		[Test()]
+		public void Test43Yields43()
+		{
+			Assert.AreEqual(Buzzer.Buzz(43), "43");
+		}
+		[Test()]
+
+
+
+		/* Border case tests */
+		public void Test1()
+		{
+			Assert.AreEqual(Buzzer.Buzz(1), "1");
+		}
+		public void Test100()
+		{
+			Assert.AreEqual(Buzzer.Buzz(100), "Buzz");
+		}
+
+
+
+		/* Additional tests */
+		public void Test0YieldsFizzBuzz()
+		{
+			Assert.AreEqual(Buzzer.Buzz(0), "FizzBuzz");
+		}
+		public void TestNegative15YieldsFizzBuzz()
+		{
+			Assert.AreEqual(Buzzer.Buzz(-15), "FizzBuzz");
+		}
+		public void Test102YieldsFizz()
+		{
+			Assert.AreEqual(Buzzer.Buzz(102), "Fizz");
 		}
 	}
 }
