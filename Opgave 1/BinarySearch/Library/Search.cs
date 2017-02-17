0fffed051fa409ef04f9921b07742339b7108c30
Garox01 <spamspamson9@gmail.com>
2017-02-17 14:11:22 +0100

    Det er snart færdigt!
    JADASDASD

--- Opgave 1/BinarySearch/Library/Search.cs
+++ Opgave 1/BinarySearch/Library/Search.cs
@@ -6,15 +6,24 @@ namespace Library
     {
         public static int Binary (IComparable[] array, IComparable target)
         {
+            // Checks if the target is lesser or greater than the smallest
+            // and greatest value in the array.
+            IComparable first = array [0];
+            IComparable last = array [(array.Length - 1)];
+            if ((target.CompareTo (first) < 0) || (target.CompareTo (last) > 0))
+            {
+                Console.WriteLine ("Target is lesser or greater than " +
+                                    "the first or last element in the array.");
+            }
             int low = 0;
             int high = array.Length - 1;
 
             while (low <= high)
             {
+                ComparisonCountedInt.HardComparisonIntBin++;// This is where we count!
                 int mid = low + (high - low) / 2;
                 IComparable midVal = array[mid];
                 int relation = midVal.CompareTo (target);
-                ComparisonCountedInt.CountComparisons++;// Er det her den der counter ting skal være fra opgave 5.13???
                 if (relation < 0)
                 {
                     low = mid + 1;
@@ -31,5 +40,21 @@ namespace Library
 
             return -1;
         }
+
+        public static int Linear (IComparable [] array, IComparable target)
+        {
+            int length = array.Length - 1;
+
+            for (int i = 0; i <= length; i++) {
+                if (array [i].CompareTo (target) == 0) {
+                    return i;
+                }
+                ComparisonCountedInt.HardComparisonIntLin++;
+
+            }
+            return -1;
+        }
+
+
     }
 }
--- Opgave 1/BinarySearch/Tests/Test.cs
+++ Opgave 1/BinarySearch/Tests/Test.cs
@@ -12,14 +12,14 @@ namespace Tests
         [Test ()]
         public void TestTooLow ()
         {
-            IComparable[] arr = gen.NextArray (10, 10);
+            IComparable [] arr = gen.NextArray (10, 10);
             Assert.AreEqual (Search.Binary (arr, -40), -1, "TestTooLow1");
         }
         [Test ()]
         public void TestTooHigh ()
         {
             IComparable [] arr = gen.NextArray (10, 10);
-            Assert.AreEqual(Search.Binary(arr, 40), -1, "TestTooHigh1");
+            Assert.AreEqual (Search.Binary (arr, 40), -1, "TestTooHigh1");
         }
         [Test ()]
         public void TestElement ()
@@ -31,12 +31,53 @@ namespace Tests
         [Test ()]
         public void TestEmptyArray ()
         {
-            IComparable[] arr = new IComparable[] { };
+            IComparable [] arr = new IComparable [] { };
 
-            for (int i = -100; i <= 100; i++) {
-                Assert.AreEqual (Search.Binary (arr, i) == -1, true, 
+            for (int i = -100; i <= 100; i++) 
+            {
+                Assert.AreEqual (Search.Binary (arr, i) == -1, true,
                                  "TestEmptyArray, i = {0}", i);
             }
         }
+
+        [Test ()]
+        public void TestBin ()
+        {
+            for (int i = 1; i <= 5000; i++) 
+            {
+                Console.WriteLine (ComparisonCountedInt.HardComparisonIntBin);
+                IComparable [] arr = gen.NextArray (10, 10);
+                Search.Binary (arr, 11);
+                Assert.LessOrEqual (ComparisonCountedInt.HardComparisonIntBin, Math.Ceiling (Math.Log (10, 2))*i, "Binary Search doesn't works!");
+            }
+
+        }
+        [Test ()]
+        public void TestLin ()
+        {
+            for (int i = 1; i <= 5000; i++) {
+                Console.WriteLine (ComparisonCountedInt.HardComparisonIntLin);
+                IComparable [] arr = gen.NextArray (10, 10);
+                Search.Linear(arr, 11);
+                Assert.LessOrEqual (ComparisonCountedInt.HardComparisonIntLin, 10 * i, "Linear Search doesn't work!");
+            }
+
+        }
+         [Test ()]
+        public void TestLinvsBin ()
+        {
+            for (int i = 1; i <= 100; i++) {
+                Console.WriteLine (ComparisonCountedInt.HardComparisonIntLin);
+                IComparable [] arr = gen.NextArray (10000, 10);
+                Search.Linear (arr, 11);
+                Search.Binary (arr, 11);
+                Assert.LessOrEqual (ComparisonCountedInt.HardComparisonIntBin, ComparisonCountedInt.HardComparisonIntLin, "Binary search is not more effektive");
+            }
+
+        }
+
+
+
     }
+
 }
